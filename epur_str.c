4d8f7e5ec6ca9acf28bbb28af7cb0ed927891675
Maksim Kramer <mkramer@c1r1p4.42quebec.com>
2023-04-04 17:09:27 -0400

    add descriptions

--- epur_str.c
+++ epur_str.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 int main(int ac, char **av)
 {
-	int i, flg;
+	int flg;
 
 	if (ac == 2)
 	{
-		i = 0;
+		int i = 0;
 		while (av[1][i] == ' ' || av[1][i] == '\t')
 			i++;
 		while (av[1][i])
