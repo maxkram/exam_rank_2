767ad5627222671abcaa9423e4189f3dfa248cc4
maxkram <max.kramer@live.com>
2023-09-13 20:54:33 -0400

    changed do_op

--- _test.c
+++ _test.c
@@ -1,26 +1,24 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int ac, char **av)
 {
-    if (ac == 2)
+    if (ac == 4)
     {
-        int i = 1;
-        int num = atoi(av[1]);
-        if (num == 1)
-            printf("1");
-        while (num >= ++i)
-        {
-            if (num % i == 0)
-            {
-                printf("%d", i);
-                if (num == i)
-                    break;
-                printf("*");
-                num /= i;
-                i = 1; 
-            }
-        }
+        int n1 = atoi(av[1]);
+        int n2 = atoi(av[3]);
+
+        if (av[2][0] == '+')
+            printf("%d", (n1 + n2));
+        if (av[2][0] == '-')
+            printf("%d", (n1 - n2));
+        if (av[2][0] == '*')
+            printf("%d", (n1 * n2));
+        if (av[2][0] == '/')
+            printf("%d", (n1 / n2));
+        if (av[2][0] == '%')
+            printf("%d", (n1 % n2));
     }
     printf("\n");
     return (0);
--- do_op.c
+++ do_op.c
@@ -1,36 +1,26 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int ac, char **av)
 {
-	if (ac == 4)
-	{
-		int n1 = atoi(av[1]);
-		int n2 = atoi(av[3]);
+    if (ac == 4)
+    {
+        int n1 = atoi(av[1]);
+        int n2 = atoi(av[3]);
 
-		switch (av[2][0])
-		{
-		case '+':
-			printf("%d", n1 + n2);
-			break;
-		case '-':
-			printf("%d", n1 - n2);
-			break;
-		case '*':
-			printf("%d", n1 * n2);
-			break;
-		case '/':
-			printf("%d", n1 / n2);
-			break;
-		case '%':
-			printf("%d", n1 % n2);
-			break;
-		default:
-			break;
-		}
-	}
-	printf("\n");
-	return (0);
+        if (av[2][0] == '+')
+            printf("%d", (n1 + n2));
+        if (av[2][0] == '-')
+            printf("%d", (n1 - n2));
+        if (av[2][0] == '*')
+            printf("%d", (n1 * n2));
+        if (av[2][0] == '/')
+            printf("%d", (n1 / n2));
+        if (av[2][0] == '%')
+            printf("%d", (n1 % n2));
+    }
+    printf("\n");
+    return (0);
 }
 
 // Assignment name  : do_op
