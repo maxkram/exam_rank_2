76d92238d08f57d371715566a92008fd5e3a8db8
mkramer <maxkram@pm.me>
2023-09-24 21:10:08 -0400

    change ft_atoi_base to more concise way

--- _test.c
+++ _test.c
@@ -1,16 +1,36 @@
-#include <stdio.h>
-
-unsigned char swap_bits(unsigned char octet)
+int		ft_atoi_base(const char *str, int str_base)
 {
-	return ((octet >> 4) | (octet << 4));
+	int i = 0;
+	int n = 1;
+	int res = 0;
+	if (str[i] == '-')
+	{
+		n = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		res *= str_base;
+		if (str[i] >= '0' && str[i] <= '9')
+			res += str[i] - 48;
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			res += str[i] - 55;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			res += str[i] - 87;	
+		i++;
+	}
+	return (res);
 }
+
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(void)
 {
-	char t = 't';
-	char G = 'G';
-
-	printf("letter_t after swap: %c\n", swap_bits(t));
-	printf("letter_G after swap: %c\n", swap_bits(G));
+	printf("%d\n", ft_atoi_base("011", 2));
+	printf("%d\n", ft_atoi_base("16", 8));
+	printf("%d\n", ft_atoi_base("123", 10));
+	printf("%d\n", ft_atoi_base("12fdb3", 16));
+	printf("%d\n", ft_atoi_base("12FDB3", 16));
 	return (0);
 }
--- ft_atoi_base.c
+++ ft_atoi_base.c
@@ -1,44 +1,81 @@
-char	to_lower(char c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (c + ('a' - 'A'));
-	return (c);
-}
+// char	to_lower(char c)
+// {
+// 	if (c >= 'A' && c <= 'Z')
+// 		return (c + ('a' - 'A'));
+// 	return (c);
+// }
 
-int get_digit(char c, int digits_in_base)
-{
-	int max_digit;
-	if (digits_in_base <= 10)
-		max_digit = digits_in_base + '0';
-	else
-		max_digit = digits_in_base - 10 + 'a';
+// int get_digit(char c, int digits_in_base)
+// {
+// 	int max_digit;
+// 	if (digits_in_base <= 10)
+// 		max_digit = digits_in_base + '0';
+// 	else
+// 		max_digit = digits_in_base - 10 + 'a';
 
-	if (c >= '0' && c <= '9' && c <= max_digit)
-		return (c - '0');
-	else if (c >= 'a' && c <= 'f' && c <= max_digit)
-		return (10 + c - 'a');
-	else
-		return (-1);
-}
+// 	if (c >= '0' && c <= '9' && c <= max_digit)
+// 		return (c - '0');
+// 	else if (c >= 'a' && c <= 'f' && c <= max_digit)
+// 		return (10 + c - 'a');
+// 	else
+// 		return (-1);
+// }
 
-int ft_atoi_base(const char *str, int str_base)
+// int ft_atoi_base(const char *str, int str_base)
+// {
+// 	int result = 0;
+// 	int sign = 1;
+// 	int digit;
+
+// 	if (*str == '-')
+// 	{
+// 		sign = -1;
+// 		++str;
+// 	}
+
+// 	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
+// 	{
+// 		result = result * str_base + (digit * sign);
+// 		++str;
+// 	}
+// 	return (result);
+// }
+
+// #include <stdlib.h>
+// #include <stdio.h>
+
+// int main(void)
+// {
+// 	printf("%d\n", ft_atoi_base("011", 2));
+// 	printf("%d\n", ft_atoi_base("16", 8));
+// 	printf("%d\n", ft_atoi_base("123", 10));
+// 	printf("%d\n", ft_atoi_base("FF", 16));
+// 	return (0);
+// }
+
+int		ft_atoi_base(const char *str, int str_base)
 {
+	int i = 0;
+	int n = 1;
 	int result = 0;
-	int sign = 1;
-	int digit;
 
-	if (*str == '-')
+	if (str[i] == '-')
 	{
-		sign = -1;
-		++str;
+		n = -1;
+		i++;
 	}
-
-	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
+	while (str[i])
 	{
-		result = result * str_base + (digit * sign);
-		++str;
+		result *= str_base;
+		if (str[i] >= '0' && str[i] <= '9')
+			result += str[i] - 48;
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			result += str[i] - 55;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			result += str[i] - 87;
+		i++;
 	}
-	return (result);
+	return (result * n);
 }
 
 #include <stdlib.h>
