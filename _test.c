f1854a08cb2d4e0b1f6507ca61048086b9554511
maxkram <max.kramer@live.com>
2023-09-26 00:36:48 -0400

    change ft_atoi_base

--- _test.c
+++ _test.c
@@ -1,36 +1,43 @@
-int		ft_atoi_base(const char *str, int str_base)
+char	to_lower(char c)
 {
-	int i = 0;
-	int n = 1;
-	int res = 0;
-	if (str[i] == '-')
-	{
-		n = -1;
-		i++;
-	}
-	while (str[i])
-	{
-		res *= str_base;
-		if (str[i] >= '0' && str[i] <= '9')
-			res += str[i] - 48;
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			res += str[i] - 55;
-		else if (str[i] >= 'a' && str[i] <= 'z')
-			res += str[i] - 87;	
-		i++;
-	}
-	return (res);
+	if (c >= 'A' && c <= 'Z')
+		return (c + ('a' - 'A'));
+	return (c);
 }
 
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(void)
+int		get_digit(char c, int digits_in_base)
 {
-	printf("%d\n", ft_atoi_base("011", 2));
-	printf("%d\n", ft_atoi_base("16", 8));
-	printf("%d\n", ft_atoi_base("123", 10));
-	printf("%d\n", ft_atoi_base("12fdb3", 16));
-	printf("%d\n", ft_atoi_base("12FDB3", 16));
-	return (0);
+	int max_digit;
+	if (digits_in_base <= 10)
+		max_digit = digits_in_base + '0';
+	else
+		max_digit = digits_in_base - 10 + 'a';
+
+	if (c >= '0' && c <= '9' && c <= max_digit)
+		return (c - '0');
+	else if (c >= 'a' && c <= 'f' && c <= max_digit)
+		return (10 + c - 'a');
+	else
+		return (-1);
+}
+
+int		ft_atoi_base(const char *str, int str_base)
+{
+	int result = 0;
+	int sign = 1;
+	int digit;
+
+	if (*str == '-')
+	{
+		sign = -1;
+		++str;
+	}
+
+	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
+	{
+		result = result * str_base;
+		result = result + (digit * sign);
+		++str;
+	}
+	return (result);
 }
--- ft_atoi_base.c
+++ ft_atoi_base.c
@@ -1,92 +1,53 @@
-// char	to_lower(char c)
-// {
-// 	if (c >= 'A' && c <= 'Z')
-// 		return (c + ('a' - 'A'));
-// 	return (c);
-// }
-
-// int get_digit(char c, int digits_in_base)
-// {
-// 	int max_digit;
-// 	if (digits_in_base <= 10)
-// 		max_digit = digits_in_base + '0';
-// 	else
-// 		max_digit = digits_in_base - 10 + 'a';
-
-// 	if (c >= '0' && c <= '9' && c <= max_digit)
-// 		return (c - '0');
-// 	else if (c >= 'a' && c <= 'f' && c <= max_digit)
-// 		return (10 + c - 'a');
-// 	else
-// 		return (-1);
-// }
-
-// int ft_atoi_base(const char *str, int str_base)
-// {
-// 	int result = 0;
-// 	int sign = 1;
-// 	int digit;
-
-// 	if (*str == '-')
-// 	{
-// 		sign = -1;
-// 		++str;
-// 	}
-
-// 	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
-// 	{
-// 		result = result * str_base + (digit * sign);
-// 		++str;
-// 	}
-// 	return (result);
-// }
-
-// #include <stdlib.h>
-// #include <stdio.h>
-
-// int main(void)
-// {
-// 	printf("%d\n", ft_atoi_base("011", 2));
-// 	printf("%d\n", ft_atoi_base("16", 8));
-// 	printf("%d\n", ft_atoi_base("123", 10));
-// 	printf("%d\n", ft_atoi_base("FF", 16));
-// 	return (0);
-// }
-
-int		ft_atoi_base(const char *str, int str_base)
+char	to_lower(char c)
 {
-	int i = 0;
-	int n = 1;
-	int result = 0;
+	if (c >= 'A' && c <= 'Z')
+		return (c + ('a' - 'A'));
+	return (c);
+}
 
-	if (str[i] == '-')
+int get_digit(char c, int digits_in_base)
+{
+	int max_digit;
+	if (digits_in_base <= 10)
+		max_digit = digits_in_base + '0';
+	else
+		max_digit = digits_in_base - 10 + 'a';
+
+	if (c >= '0' && c <= '9' && c <= max_digit)
+		return (c - '0');
+	else if (c >= 'a' && c <= 'f' && c <= max_digit)
+		return (10 + c - 'a');
+	else
+		return (-1);
+}
+
+int ft_atoi_base(const char *str, int str_base)
+{
+	int result = 0;
+	int sign = 1;
+	int digit;
+
+	if (*str == '-')
 	{
-		n = -1;
-		i++;
+		sign = -1;
+		++str;
 	}
-	while (str[i])
+
+	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
 	{
-		result *= str_base;
-		if (str[i] >= '0' && str[i] <= '9')
-			result += str[i] - 48;
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			result += str[i] - 55;
-		else if (str[i] >= 'a' && str[i] <= 'z')
-			result += str[i] - 87;
-		i++;
+		result = result * str_base + (digit * sign);
+		++str;
 	}
-	return (result * n);
+	return (result);
 }
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void)
+int		main(int ac, char **av)
 {
-	printf("%d\n", ft_atoi_base("011", 2));
-	printf("%d\n", ft_atoi_base("16", 8));
-	printf("%d\n", ft_atoi_base("123", 10));
-	printf("%d\n", ft_atoi_base("FF", 16));
+	if (ac == 3)
+		printf("result: %d\n", ft_atoi_base(av[1], atoi(av[2])));//
 	return (0);
 }
 
