5078353fe09cd0c8e1717adc04c4eddccd3e0f89
maxkram <max.kramer@live.com>
2023-09-28 21:18:44 -0400

    restore atoi base

--- _test.c
+++ _test.c
@@ -1,58 +1,33 @@
-#include "sort_list.h"
-#include <stdlib.h>
 
-t_list *sort_list(t_list *lst, int (*cmp)(int, int))
-{
-	if (!lst || !lst->next)
-		return (lst);
-	t_list *curr = lst;
-	while (curr->next)
-	{
-		if (cmp(curr->data, curr->next->data) == 0)
-		{
-			int tmp = curr->data;
-			curr->data = curr->next->data;
-			curr->next->data = tmp;
-			curr = lst;
-		}
-		else
-			curr = curr->next;
-	}
-	return (lst);
-}
 
 #include <stdio.h>
 
-t_list *add_int(t_list *list, int nb)
+void sort_int_tab(int *tab, unsigned int size)
 {
-	t_list *new = (t_list *)malloc(sizeof(t_list));
-	new->data = nb;
-	new->next = list;
-	return (new);
-}
-
-int ascending(int a, int b)
-{
-	return (a <= b);
+	unsigned int i = 0;
+	int tmp;
+	while (i < (size - 1))
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+			i = 0;
+		}
+		else
+			i++;
+	}
 }
 
 int main(void)
 {
-	t_list *list;
+    int a[6] = {9, 7, 6, 4, 5, 10};
+    int i = 0;
+    int size = 6;
 
-	list = NULL;
-	list = add_int(list, 9);
-	list = add_int(list, 3);
-	list = add_int(list, 2);
-	list = add_int(list, 4);
-	list = add_int(list, 1);
-	list = sort_list(list, &ascending);
-
-	while (list != NULL)
-	{
-		printf("%d\n", list->data);
-		list = list->next;
-	}
-
-	return (0);
+    sort_int_tab(a, size);
+    while (i < size)
+        printf("%d\n", a[i++]);
+    return (0);
 }
