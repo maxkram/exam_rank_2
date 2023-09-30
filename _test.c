c39528fe2882d39ee19919a78dac7f7cb9e2b939
mkramer <maxkram@pm.me>
2023-09-29 20:52:27 -0400

    change test

--- _test.c
+++ _test.c
@@ -2,40 +2,18 @@
 #include <string.h>
 #include <unistd.h>
 
-// int main(int ac, char **av)
-// {
-// 	if (ac == 2)
-// 	{
-
-// 	}
-// 	write(1, "\n", 1);
-// 	return (0);
-// }
-
-void ft_putnbr(int n)
+int is_space(char c)
 {
-	if (n > 9)
-		ft_putnbr(n / 10);
-	write(1, &"0123456789"[n % 10], 1);
+	return (c == 32 || c == 9);
 }
 
-int main(void)
+int main(int ac, char **av)
 {
-	int i = 1;
-
-	while (i < 101)
+	if (ac == 2)
 	{
-		if (i % 15 == 0)
-			write(1, "fizzbuzz", 8);
-		else if (i % 5 == 0)
-			write(1, "buzz", 4);
-		else if (i % 3 == 0)
-			write(1, "fizz", 4);
-		else
-			ft_putnbr(i);
-		i++;
-		write(1, "\n", 1);
-	}
+		int i = 0;
 
+	}
+	write(1, "\n", 1);
 	return (0);
 }
