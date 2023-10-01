fa34b79662bed48d48bee124a3a39bf669e3da8b
mkramer <maxkram@pm.me>
2023-10-01 13:40:28 -0400

    update epur_str

--- 3-0_epur_str.c
+++ 3-0_epur_str.c
@@ -3,25 +3,25 @@ int is_space(char c)
 {
 	return (c == 32 || c == 9);
 }
-int main(int argc, char const *argv[])
+int main(int ac, char const *av[])
 {
 	int flg;
 
-	if (argc == 2)
+	if (ac == 2)
 	{
 		int i = 0;
-		while (is_space(argv[1][i]))
+		while (is_space(av[1][i]))
 			i++;
-		while (argv[1][i])
+		while (av[1][i])
 		{
-			if (is_space(argv[1][i]))
+			if (is_space(av[1][i]))
 				flg = 1;
-			if (!(is_space(argv[1][i])))
+			if (!(is_space(av[1][i])))
 			{
 				if (flg)
 					write(1, " ", 1);
 				flg = 0;
-				write(1, &argv[1][i], 1);
+				write(1, &av[1][i], 1);
 			}
 			i++;
 		}
