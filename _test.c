6c8061fca82212bd0fc2e5914321d6e17d116d18
maxkram <max.kramer@live.com>
2023-10-09 14:17:30 -0400

    update strcmp

--- _test.c
+++ _test.c
@@ -3,27 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t	ft_strcspn(const char *s, const char *reject)
+int ft_strcmp(char *s1, char *s2)
 {
-	const char *s1 = s;
-	while(*s1++)
-	{
-		const char *s2=reject;
-		while(*s2)
-			if(*s1==*s2++)
-				return (s1-s);
-	}
-	return (s1 - s);
+	int i = 0;
+	while(s1[i]&&s2[i]&&s1[i]==s2[i])
+		i++;
+	return (s1[i]-s2[i]);
 }
 
 int main()
 {
-	const char str1[] = "ABCDEF4960910";
-	const char str2[] = "013";
-
-	int len = strcspn(str1, str2);
-	int len1 = ft_strcspn(str1, str2);
-
-	printf("%d\n", len + 1);
-	printf("%d\n", len1 + 1);
+	char s1[]="abcdef";
+	char s2[]="ABCDEF";
+	int r1 = strcmp(s1,s2);
+	int r2 = ft_strcmp(s1,s2);
+	printf("%d\n",r1);
+	printf("%d\n",r2);
+	return 0;
 }
