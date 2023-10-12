1055288f8aa31b3d00e02e0044ddcfab07ae8e06
mkramer <maxkram@pm.me>
2023-10-12 19:27:08 -0400

    checked

--- _test.c
+++ _test.c
@@ -1,26 +1,23 @@
 #include <unistd.h>
 
-void putnbr42(int n)
+void last_word(char *str)
 {
-	if(n > 9)
-		putnbr42(n/10);
-	write(1, &"0123456789"[n%10], 1);
+	int i = 0;
+	int j = 0;
+	while(str[i])
+	{
+		if(str[i]==32&&(str[i+1]>32&&str[i+1]<127))
+			j=i+1;
+		i++;
+	}
+	while(str[j]>32&&str[j]<127)
+		write(1, &str[j++], 1);
 }
 
 int main(int ac, char **av)
 {
-	int i = 0;
-	while(i++ < 101)
-	{
-		if(i%15==0)
-			write(1, "fizzbuzz", 8);
-		else if(i%5==0)
-			write(1, "buzz", 4);
-		else if(i%3==0)
-			write(1, "fizz", 4);
-		else
-			putnbr42(i);
-		write(1, "\n", 1);
-	}
+	if(ac==2)
+		last_word(av[1]);
+	write(1, "\n", 1);
 	return 0;
 }
